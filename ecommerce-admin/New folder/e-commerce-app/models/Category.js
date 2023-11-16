526e053c328d385d34863d558cb3fcdae54df283
RajarshiG <76178523+RajG98@users.noreply.github.com>
2023-11-16 20:15:36 +0530

    Delete ecommerce-admin directory

--- ecommerce-admin/New folder/e-commerce-app/models/Category.js
+++
@@ -1,9 +0,0 @@
-const  mongoose = require("mongoose");
-
-
-
-const CategorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    parent:{type:mongoose.Types.ObjectId,ref:'Category'},
-})
-export const Category = mongoose.models.Category || mongoose.model('Category', CategorySchema);
