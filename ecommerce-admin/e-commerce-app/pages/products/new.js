8505812539ac6c6b665c6287609816301b33e834
RajarshiG <76178523+RajG98@users.noreply.github.com>
2023-11-16 19:53:09 +0530

    Delete ecommerce-admin/e-commerce-app directory

--- ecommerce-admin/e-commerce-app/pages/products/new.js
+++
@@ -1,9 +0,0 @@
-import ProductForm from "@/components/ProductForm";
-import Layout from "@/components/layout";
-
-export default function NewProduct() {
-  return <Layout>
-    <h1>New Product</h1>
-    <ProductForm />
-  </Layout>
-}
